df7fd673e29c641664a332212f7cfce683e38f11
tomastrajan <tomas.trajan@gmail.com>
2019-10-13 22:29:49 +0200

    feat(website): many styles, content, images, ...

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -1,7 +1,7 @@
 "use strict";
 
 function updateTime() {
-    const fut = new moment('2020-12-31');
+    const fut = new moment('2020-06-01');
     const now = new moment();
     const duration = moment.duration(fut.diff(now));
 
@@ -18,5 +18,6 @@ function updateTime() {
 }
 
 if(document.querySelector('.countdown')) {
+    updateTime();
     setInterval(updateTime, 1000);
 }
